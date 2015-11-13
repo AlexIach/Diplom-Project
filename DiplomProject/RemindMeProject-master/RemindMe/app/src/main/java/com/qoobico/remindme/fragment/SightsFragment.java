25ca5cda1ffdff44e42a61987e9118ec39efd4ac
Iachimov Alexandru <alex.iachimov@gmail.com>
2015-11-13 21:34:11 +0200

    First commit

---
+++ DiplomProject/RemindMeProject-master/RemindMe/app/src/main/java/com/qoobico/remindme/fragment/SightsFragment.java
@@ -0,0 +1,31 @@
+package com.qoobico.remindme.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qoobico.remindme.R;
+
+public class SightsFragment extends Fragment {
+    private static final int LAYOUT = R.layout.sights_fragment;
+
+    private View view;
+
+    public static SightsFragment getInstance() {
+        Bundle args = new Bundle();
+        SightsFragment fragment = new SightsFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(LAYOUT, container, false);
+        return view;
+    }
+}
---
+++ DiplomProject/RemindMeProject-master/RemindMe/app/src/main/java/com/qoobico/remindme/other/WsConfig.java
@@ -0,0 +1,8 @@
+package com.qoobico.remindme.other;
+
+/**
+ * Created by ЦАРЬ on 30.09.2015.
+ */
+public class WsConfig {
+    public static final String URL_WEBSOCKET = "ws://192.168.0.33:8080/Sample_Web_Project1/chat?name=";
+}
